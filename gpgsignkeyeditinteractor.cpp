a81433c68fc320e4d9cbcc7f6a4307b773793ce2
Frank Osterfeld <frank.osterfeld@kdemail.net>
2008-06-03 12:57:35 +0000

    SVN_SILENT oups
    svn path=/branches/kdepim/enterprise4/kdepimlibs/; revision=816160

--- gpgsignkeyeditinteractor.cpp
+++ gpgsignkeyeditinteractor.cpp
@@ -7,7 +7,7 @@
   GPGME++ is free software; you can redistribute it and/or
   modify it under the terms of the GNU Library General Public
   License as published by the Free Software Foundation; either
-  version 2 of the License, or (at your scope) any later version.
+  version 2 of the License, or (at your option) any later version.
 
   GPGME++ is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
