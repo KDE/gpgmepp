1f2819f48ddf247fc3331cfeaf00b1f0001f9511
Thomas McGuire <mcguire@kde.org>
2009-07-30 16:19:28 +0000

    Compile with older gpgme versions
    svn path=/branches/kdepim/enterprise4/kdepimlibs/; revision=1004627

--- key.cpp
+++ key.cpp
@@ -256,6 +256,7 @@ namespace GpgME {
 #endif
       me->keylist_mode     |= him->keylist_mode;
 
+#ifdef HAVE_GPGME_SUBKEY_T_IS_CARDKEY
       // make sure the gpgme_sub_key_t::is_cardkey flag isn't lost:
       for ( gpgme_sub_key_t mysk = me->subkeys ; mysk ; mysk = mysk->next )
           for ( gpgme_sub_key_t hissk = him->subkeys ; hissk ; hissk = hissk->next )
@@ -263,6 +264,7 @@ namespace GpgME {
                   mysk->is_cardkey |= hissk->is_cardkey;
                   break;
               }
+#endif
 
       return *this;
   }
